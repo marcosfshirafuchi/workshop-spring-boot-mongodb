dd2f2a2efab2715ebaf0617fe198537e3ab47ae4
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-08-18 22:11:36 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/marcosshirafuchi/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.marcosshirafuchi.workshopmongo.repository;
+
+import com.marcosshirafuchi.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
