eadecfc02e7f9c6af4f313f280f010bf845c406a
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-08-21 22:49:19 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/marcosshirafuchi/workshopmongo/services/PostService.java
+++ src/main/java/com/marcosshirafuchi/workshopmongo/services/PostService.java
@@ -10,10 +10,9 @@ import java.util.Optional;
 
 @Service
 public class PostService {
+
     @Autowired
     private PostRepository repo;
-
-
 
     public Post findById(String id){
         Optional<Post> post = repo.findById(id);
