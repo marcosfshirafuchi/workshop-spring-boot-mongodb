05e7a627257a5e89820ffad0fb5da86908bcba88
Marcos Ferreira Shirafuchi <marcosfshirafuchi@gmail.com>
2025-08-21 21:59:49 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/marcosshirafuchi/workshopmongo/dto/AuthorDTO.java
+++ src/main/java/com/marcosshirafuchi/workshopmongo/dto/AuthorDTO.java
@@ -18,4 +18,20 @@ public class AuthorDTO implements Serializable {
         id = obj.getId();
         name = obj.getName();
     }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/marcosshirafuchi/workshopmongo/resources/UserResource.java
+++ src/main/java/com/marcosshirafuchi/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.marcosshirafuchi.workshopmongo.resources;
 
+import com.marcosshirafuchi.workshopmongo.domain.Post;
 import com.marcosshirafuchi.workshopmongo.domain.User;
 import com.marcosshirafuchi.workshopmongo.dto.UserDTO;
 import com.marcosshirafuchi.workshopmongo.services.UserService;
@@ -53,5 +54,17 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+//    @RequestMapping(value = "/{id}/posts" ,method = RequestMethod.GET)
+//    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+//        User obj = service.findById(id);
+//        return ResponseEntity.ok().body(obj.getPosts());
+//    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
